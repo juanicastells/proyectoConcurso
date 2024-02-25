693cc91e8f29af4048a94f548d22010cadabd186
juanicastells <juan-c65@live.com.ar>
2024-02-25 11:39:21 -0300

    Solución de errores al cargar estilos en UsuarioActualizado.html

--- demo/src/main/java/com/example/demo/controller/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.business.UsuarioBusiness;
 import com.example.demo.entity.Usuario;
@@ -19,6 +17,22 @@ public class UsuarioController {
 
     @Autowired
     UsuarioBusiness usuarioBusiness;
+
+    @GetMapping("/registro")
+    public String registro() {
+        return "registro";
+    }
+
+    @GetMapping("/usuarioRegistrado")
+    public String usuarioRegistrado() {
+        return "usuarioRegistrado";
+    }
+
+    @PostMapping("/registro")
+    public String altaUsuario(Usuario usuario) {
+        usuarioBusiness.insertUsuario(usuario);
+        return "usuarioRegistrado";  
+    }
 
     @GetMapping("/listaUsuarios")
     public String mostrarListaDeUsuarios(Model model) {
